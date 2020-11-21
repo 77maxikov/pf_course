6b63d667c08186dac95f5ba8a92dfe6b13cbaff5
77maxikov <77maxikov@mail.ru>
2020-11-21 11:51:28 +0300

    Added dictionary sample

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,98 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 
 using namespace std;
 
 int main(int argc,char* argv[])
-{
-    if (argc < 3) {
+{/*
+    struct S{
+        char *name;
+        int height;
+        char countrycode;
+    };
+
+    S s ;//= {"John",56,'G'};
+    s.name = new char[10];
+    cout << "Name" << s.name << '\t' << "height " << s.height << endl;
+    //strcpy(s.name,"John");
+    //strcpy(s.name,"Alexander");
+    cin >> s.name ;
+    cout << "Name" << s.name << '\t' << "height " << s.height << endl;
+
+    S group[20];
+    group[0].name = new char[6];
+    cin >> group[0].name;
+    group[1].height = 170;
+
+    S* newgroup;
+    newgroup = new S[18];
+    newgroup[0].height = 167;
+    (*newgroup).countrycode = 'A';
+    newgroup->countrycode = 'R';
+    (newgroup + 2)->countrycode ='U';
+
+    struct M{
+        char disstheme[30];
+        S s_info;
+    };*/
+
+    //M m = {"Development", {,170,'U'}};
+    /*struct Translation{
+        char ru[81];
+        char en[81];
+    };*/
+    struct Translation{
+        char *ru;
+        char *en;
+    };
+
+    struct Array{
+        size_t size;
+        Translation *data;
+    };
+
+    Array transstorage = {0,nullptr};
+
+    if (transstorage.size == 0){
+        transstorage.data = new Translation;
+        transstorage.size = 1;
+        char buffer[81];
+        cout << "Input english word" << endl;
+        cin >> buffer;
+        size_t k = 0;
+        for (;k < 81;++k)
+            if ( buffer[k] == '\0' )
+                break;
+        cout << k << " bytes " << endl;
+        transstorage.data->en = new char[k+1];
+        for ( size_t counter = 0; counter < k+1 ;++counter )
+            transstorage.data->en[counter] = buffer[counter];
+
+        cout << "Input russian word" << endl;
+        cin >> buffer;
+        k = 0;
+        for (;k < 81;++k)
+            if ( buffer[k] == '\0' )
+                break;
+        cout << k << " bytes " << endl;
+        transstorage.data->ru = new char[k+1];
+        for ( size_t counter = 0; counter < k+1 ;++counter )
+            transstorage.data->ru[counter] = buffer[counter];
+    }
+    cout << "Dictionary size " << transstorage.size << endl;
+    cout << "First element " << transstorage.data[0].en << ':' << transstorage.data[0].ru  << endl;
+
+    delete[] transstorage.data[0].en;
+    delete[] transstorage.data[0].ru;
+    delete transstorage.data;
+
+
+    //s.name = new char[5];
+
+    //cout << "Sizeof(s) " << sizeof(s) << endl;
+
+    /*if (argc < 3) {
         cout << "Not enough parameters" << endl;
         return 1;
     }
@@ -27,7 +114,7 @@ int main(int argc,char* argv[])
         return 0;
     }
     return 1;
-
+*/
 
     /*
     fstream fs;
